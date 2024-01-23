28f059d55c0088bd9b868ccee7e7ea5a92e45bdf
Roman Njoroge <roman.njoroge@njuguna.com>
2024-01-23 19:23:02 +0300

    changing name of game

--- GameStart.m
+++ GameStart.m
@@ -2,9 +2,10 @@
 % see dependency report for input output
 clear
 clc
-play = questdlg("Do you want to play?",'60MILES', "Yes",...
+play = questdlg("Do you want to play?",'Chicken Aid', "Yes",...
     "No", "Yes")
 switch play
+    
     case "Yes"
     player = imread('All_Sprites_PNG\temp_chick.png');
     start = msgbox('Chicken lil is lost in the woods. The nearest civilization is 60 miles away. Help him in his journey to saftey!', 'The story!', 'custom',...
